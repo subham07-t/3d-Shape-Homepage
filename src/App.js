fe82d8a41547ca7febe1707c5fb6f270f9ca4443
subham07-t <82716446+subham07-t@users.noreply.github.com>
2022-09-15 17:01:08 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Spline from "@splinetool/react-spline";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <Spline
+        className="spline"
+        scene="https://prod.spline.design/eTqerFPtxhA4RexT/scene.splinecode"
+      />
+      <div className="content">
+        <h1>3D Shape Homepage</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor itaque
+          temporibus officia doloremque qui perferendis, beatae id voluptate,
+          minus debitis asperiores.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
-
-export default App;
